322fc24f1391fcb08dbc8d30407bd971253e21ec
name <you@example.com>
2019-04-27 11:53:11 -0700

    added file & service resource declarations to the init.pp for httpd.conf and vhost template

--- production/modules/webserver/manifests/init.pp
+++ production/modules/webserver/manifests/init.pp
@@ -1,13 +1,25 @@
-class webserver {
-  if $::osfamily == 'RedHat' {
-    package { 'httpd' :
-      ensure => present
-    }
-  } elsif $::osfamily == 'Debian' {
-    package { 'apache2' :
-      ensure => present
-    }
-  } else {
-      fail "This is not a supported distro."
-  }
+class webserver { 
+	package { 'httpd':
+		ensure => present
+	}
+
+	file { '/etc/httpd/conf/httpd.conf': 
+		ensure => file,
+		source  => 'puppet:///modules/webserver/httpd.conf',
+		require => Package['httpd'],
+	}
+	
+	file { '/etc/httpd/conf.d/vhost.conf':
+ 		ensure => file, 
+		content => template("webserver/vhost.conf.erb"),
+  		require => Package['httpd'],
+	}
+
+	service { 'httpd':
+		ensure => running,
+		enable => true,
+		hasrestart => true,
+		require => [ File['/etc/httpd/conf/httpd.conf'], File['/etc/httpd/conf/httpd.conf'] ]
+	}
+
 }
